f4eeb121035b483764d2f658a4c9a38e0074d5d1
Hind-ma <hind-mansour@windowslive.com>
2022-09-22 14:16:02 +0200

    Started working on the new page.

--- main/src/NewGameController.java
+++ main/src/NewGameController.java
@@ -1,3 +1,23 @@
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.TextFlow;
 public class NewGameController {
+
+    @FXML
+    private TextFlow TextBox;
+
+    @FXML
+    private ImageView GameImage;
+
+    @FXML
+    private ImageView Cross;
+
+    @FXML
+    private Button ChoiceButton1;
+
+    @FXML
+    private Button ChoiceButton2;
 
 }
