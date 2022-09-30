134dc40b2dc00a5e89c15feb2607f2168ff5d2c9
Jennifer <jennifer.l.hallberg@gmail.com>
2022-09-30 20:03:18 +0200

    Started to implement events!

--- main/src/Model/Forrest/Choice.java
+++ main/src/Model/Forrest/Choice.java
@@ -2,4 +2,5 @@ package Model.Forrest;
 
 //An event that occurs when the player has to make a choice to move to the next event
 public class Choice extends Event{
+
 }
--- main/src/Model/Forrest/Event.java
+++ main/src/Model/Forrest/Event.java
@@ -1,6 +1,31 @@
 package Model.Forrest;
 
+import java.util.List;
+
 //Paret class for all kinds of things that can happen in the game
 //An event is always triggered when the player moves to another part of the story
 public abstract class Event {
+    String eventText;
+    List<String> choices;
+    List<Event> choiceEvents;
+
+    public Event nextEvent(int choice)
+    {
+        return choiceEvents.get(choice);
+    }
+
+    public List<Event> getChoiceEvents()
+    {
+        return choiceEvents;
+    }
+
+    public List<String> getChoices()
+    {
+        return choices;
+    }
+
+    public String getEventText()
+    {
+        return eventText;
+    }
 }
