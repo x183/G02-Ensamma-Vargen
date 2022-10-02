85a8661bd9f014cb383a4eef6afc41c9ade4847f
Hind-ma <hind-mansour@windowslive.com>
2022-10-03 00:44:26 +0200

    Added text to the first choice

--- main/src/Controller/NewGameController.java
+++ main/src/Controller/NewGameController.java
@@ -1,26 +1,35 @@
 package Controller;
 
+import Model.Forrest.Choice;
+import Model.Forrest.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
-import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 import java.util.Objects;
 
 public class NewGameController {
 
+    Event event;
+
     @FXML
-    private TextFlow TextBox;
+    private TextArea textBox;
 
     @FXML
     private ImageView GameImage, Cross;
 
     @FXML
     private Button ChoiceButton1, ChoiceButton2, ChoiceButton3, ChoiceButton4, ExitButton;
+
+    public void initialize() {
+        event = new Choice();
+        textBox.setText(event.getEventText());
+    }
 
     public void pressedExitButton() throws Exception{
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../View/MainMenu.fxml")));
