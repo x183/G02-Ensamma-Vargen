e248bb7aeae814e78785c852dc32083732dd8f4b
Stocken <123vincentstocks@gmail.com>
2022-10-13 23:39:22 +0200

    Made simple changes to GUI for NewGameController, the idea is to create a player and then start the game in another stage. having some problems with the alignment of the warning text (warning for trying to continue without entering a name and choosing a class) but should be simple javafx stuff

--- main/src/Controller/NewGameController.java
+++ main/src/Controller/NewGameController.java
@@ -1,33 +1,90 @@
 package Controller;
 
 import Model.Events.EventSuper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
+import java.net.URL;
+import java.util.List;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class NewGameController {
+public class NewGameController implements Initializable {
 
-    EventSuper event;
+    //EventSuper event;
+    protected Text createCharacterText = new Text("Create your character");
+    private String selectedClassString = "";
+    private String selectedNameString = "";
 
     @FXML
-    private TextArea textBox;
+    private TextFlow createCharacter;
 
     @FXML
     private ImageView GameImage, Cross;
 
     @FXML
-    private Button ChoiceButton1, ChoiceButton2, ChoiceButton3, ChoiceButton4, ExitButton;
+    private Button ExitButton, submitNameButton;
 
-    public void initialize() {
-        //event = new Choice();
-        textBox.setText(event.getEventText());
+
+    @FXML
+    private ComboBox<String> classComboBox;
+
+    @FXML
+    private TextField playerName;
+    @FXML
+    private Label continueWarning;
+
+
+    @FXML
+    void selectClass() {
+        this.selectedClassString = classComboBox.getSelectionModel().getSelectedItem();
+    }
+    @FXML
+    void nameSubmit(){
+        String name = playerName.getText();
+        setName(name);
+    }
+    void setName(String name){
+        this.selectedNameString = name;
+    }
+
+    @FXML
+    private void continueButton(){
+        if(selectedNameString.equals("") && selectedClassString.equals("")){
+            setContinueWarning("You need to choose a name and pick a class!");
+
+        } else if (!selectedNameString.equals("") && selectedClassString.equals("")){
+            setContinueWarning("You need to pick a class!");
+
+        } else if (selectedNameString.equals("")){
+            setContinueWarning("You need to choose a name!");
+
+        } else
+        System.out.println(selectedNameString + " is a " + selectedClassString);
+    }
+    private void setContinueWarning(String warning){
+        continueWarning.setText(warning);
+        continueWarning.setAlignment(Pos.CENTER);
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        createCharacter = new TextFlow(createCharacterText);
+        ObservableList<String> selectClassList = FXCollections.observableArrayList("Warrior", "Hunter", "Create your own");
+        classComboBox.setItems(selectClassList);
     }
 
     public void pressedExitButton() throws Exception{
